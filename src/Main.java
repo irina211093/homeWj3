e2c00632821fe8db51a58a8ac0d8889fb15c3a37
irina211093 <irinadanilkor@gmail.com>
2024-08-28 09:29:02 +0200

    chanded mistakes

--- src/Main.java
+++ src/Main.java
@@ -27,7 +27,6 @@ public class Main {
                 if (operation == '/') {
                     System.out.println("Частное: " + arithmeticOperations.quotient());
                 }
-
             }
         }
 
